c1f0db987b340edadac7c929658a5f01af5d9777
BiSerge <bichevoi@gmail.com>
2021-11-20 17:55:07 +0600

    main branch

--- TestBranch3/Form1.Designer.cs
+++ TestBranch3/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace TestBranch3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(350, 143);
+            this.ClientSize = new System.Drawing.Size(384, 261);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
