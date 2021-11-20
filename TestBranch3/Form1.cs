b093e46f412602bfde2d90a2409e282d7c44a6b1
BiSerge <bichevoi@gmail.com>
2021-11-20 17:58:38 +0600

    added a button and a label

--- TestBranch3/Form1.cs
+++ TestBranch3/Form1.cs
@@ -12,9 +12,16 @@ namespace TestBranch3
 {
     public partial class Form1 : Form
     {
+        string str = "Hello world from Form1";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = str;
         }
     }
 }
